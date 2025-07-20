6ab5b127a5bfb16315c83782e623fb3d9a4967e9
Rogério Miranda <rm.rogerio1@gmail.com>
2025-07-20 16:56:33 -0300

    Alterar nome

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ console.log('Boas vindas');
 
 //Crie uma variável chamada "nome" e atribua a ela o seu nome. 
 //Em seguida, utilize o console.log para exibir a mensagem "Olá, [seu nome]!" no console do navegador.
-let nome = 'Rogério';
+let nome = 'Rogério M.';
 console.log(`Olá, ${nome}!`);
 
 //Crie uma variável chamada "nome" e atribua a ela o seu nome. 
